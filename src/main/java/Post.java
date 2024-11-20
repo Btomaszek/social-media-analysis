3a25b70ed18e1e857f324f7f897064ca717525ee
Btomaszek <bawana2@hawaii.edu>
2024-11-19 22:22:16 -1000

    Update Post.java

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,13 +1,19 @@
 public class Post {
     public int id;
+    public Date creationDate;
     public String content;
     public User author;
     public String timestamp;
+    List<Comment> comments; 
+    List<User> seenBy; 
 
     public Post(int id, String content, User author, String timestamp) {
         this.id = id;
         this.content = content;
         this.author = author;
         this.timestamp = timestamp;
+        this.creationDate = creationDate; 
+        this.comments = new ArrayList<>(); 
+        this.seenBy = new ArrayList<>(); 
     }
 }
